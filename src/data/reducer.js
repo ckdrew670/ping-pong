6594c4c81e36cb8a120e5bbdac33ad9d6d670ba2
Charlotte Drew <ckdrew670@hotmail.com>
2019-12-10 10:39:59 +0000

    Completed deuce service challenge - bit convoluted though, not tidy answer

--- src/data/reducer.js
+++ src/data/reducer.js
@@ -7,7 +7,7 @@ const p1 = (state) => ({ ...state, player1: state.player1 + 1 });
 const p2 = (state) => ({ ...state, player2: state.player2 + 1 });
 
 // handle server change
-const changeServer = (state) => ({ ...state, p1Serves: Math.floor((state.player1 + state.player2) / 5) % 2 === 0}); 
+const changeServer = (state) => ({ ...state, p1Serves: state.player1 + state.player2 <= 40  ? Math.floor((state.player1 + state.player2) / 5) % 2 === 0 : Math.floor((state.player1 + state.player2) / 2) % 2 === 0 }); 
 
 // handle winning message
 const winner = (state) => ({ ...state, winner: state.player1 >= 21 && state.player2 <= state.player1 - 2 || state.player2 >= 21 && state.player1 <= state.player2 - 2 });
@@ -15,8 +15,8 @@ const winner = (state) => ({ ...state, winner: state.player1 >= 21 && state.play
 const reducer = (state, action) => {
     
 	switch(action.type) {
-        case "incrementPlayer1": return winner(changeServer(p1(state)));
-        case "incrementPlayer2": return winner(changeServer(p2(state)));
+        case "incrementPlayer1": return changeServer(winner(p1(state)));
+        case "incrementPlayer2": return changeServer(winner(p2(state)));
         case "reset": return initial;
 		default: return state;
 	}
